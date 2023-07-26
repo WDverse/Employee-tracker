303d2dcd457d6360e416e5c9bad0a065c6eb0392
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-07-26 04:29:40 +0000

    installed npm inquirer

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const questions = inquirer
             type: 'list',
             name: 'task',
             message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role','Add an employee', 'update an employee role']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role','Add an employee', 'Update an employee role']
         },
     ])
