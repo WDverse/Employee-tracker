f49b21df198ad17c2249275bdc161d976911191c
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-07-29 00:57:23 +0000

    updated seeds.sql to populate database

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
-DROP DATABASE IF EXISTS employee_list;
-CREATE DATABASE employee_list;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
 USE employee_list;
 
 CREATE TABLE department(
-    id INT NOT NULL PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30) NOT NULL);
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (id, name)
+VALUES
+    ("Engineering"),
+    ("Sales"),
+    ("Finance"),
+    ("Legal");
+
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+    ("Lead Engineer", 300000, 1),
+    ("Sales Lead", 100000, 2),
+    ("Account Manager", 160000, 3),
+    ("Lawyer", 190000, 4),
+    ("Software Engineer", 200000, 1),
+    ("Legal Team Lead", 250000, 4),
+    ("Sales Person", 80000, 2),
+    ("Accountant", 125000, 3);
+
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES
+    ("John", "Doe", 2),
+    ("Mike", "Chan", 1),
+    ("Emma", "Watson", 4),
+    ("Naruto", "Uzumaki", 6),
+    ("Tom", "Allen", 8),
+    ("Cristiano", "Ronaldo", 3),
+    ("Emmanuel", "Appiagyei", 5),
+    ("Tom", "Brown", 7);
+    
