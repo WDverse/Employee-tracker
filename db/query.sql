f577634f1d3057ebcd551fcf968ca7d2e5a95900
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-02 00:26:29 +0000

    updated queries, connected db to node, updated questions in index.js

---
+++ db/query.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM department;
+
+SELECT *
+FROM role
+JOIN department ON role.department_id = department.id;
+
+
+SELECT *
+FROM employee
+JOIN role ON employee.role_id = role.id;
+
--- index.js
+++ index.js
@@ -8,6 +8,71 @@ const questions = inquirer
             type: 'list',
             name: 'task',
             message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role','Add an employee', 'Update an employee role']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        },
+
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What is the name of the department?'
+        },
+
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is the name of the role?'
+        },
+
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the role?'
+        },
+
+        {
+            type: 'input',
+            name: 'new-role',
+            message: 'Which department does the role belong to?',
+            choices: ['Finance', 'Legal', 'Sales', 'Service']
+        },
+
+        {
+            type: 'input',
+            name: 'firstname',
+            message: "What is the employee's first name?"
+        },
+
+        {
+            type: 'input',
+            name: 'lastname',
+            message: "What is the employee's last name?"
+        },
+
+        {
+            type: 'input',
+            name: 'employee-role',
+            message: "What is the employee's role?",
+            choices: ['Account Manager', 'Legal Lead', 'Sales Lead', 'Lawyer', 'Software Engineer', 'Legal Team Lead', 'Sales Person', 'Accountant',]
+        },
+
+        {
+            type: 'input',
+            name: 'manager',
+            message: "Who's the employee's manager?",
+            choices: ['Mike Chan', 'Naruto Uzumaki', 'Emmanuel Appiagyei', 'Cristiano Ronaldo', 'John Doe']
+        },
+
+        {
+            type: 'input',
+            name: 'update',
+            message: "Which employee's role do you want to update?",
+            choices: ['Mike Chan', 'Naruto Uzumaki', 'Emmanuel Appiagyei', 'Cristiano Ronaldo', 'John Doe', 'Tom Allen', 'Emma Watson', 'Tom Brown']
+        },
+
+        {
+            type: 'input',
+            name: 'update',
+            message: "Which role do you want to assign the selected employee?",
+            choices: ['Account Manager', 'Legal Lead', 'Sales Lead', 'Lawyer', 'Software Engineer', 'Legal Team Lead', 'Sales Person', 'Accountant',]
         },
     ])
