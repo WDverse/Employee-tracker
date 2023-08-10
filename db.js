977ab125ece307eb8a2c65875c84712309bb28a7
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-10 17:51:43 -0400

    added class to with empty functions to handle user choice

--- db.js
+++ db.js
@@ -1,3 +1,36 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const connection = require('./connection');
+
+class Db {
+    constructor(connection) {
+        this.connection = connection;
+    }
+    findAllDepartments() {
+
+    }
+
+    findAllRoles() {
+
+    }
+
+    findAllEmployees() {
+
+    }
+
+    addDepartment() {
+
+    }
+
+    addRole() {
+
+    }
+
+    addEmployee() {
+
+    }
+
+    updateEmployeeRole() {
+
+    }
+}
