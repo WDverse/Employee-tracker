00a61109782800a7974912c8c9ca38ad4a13629f
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-10 17:37:12 -0400

    fixed connection errors

--- connection.js
+++ connection.js
@@ -13,15 +13,3 @@ con.connect(function (err) {
 });
 
 module.exports = con;
-
-
-
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("1 record inserted");
-//   });
-// });
--- db.js
+++ db.js
@@ -1,6 +1,3 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const connection = require('./connection');
-
-const db = mysql.createConnection(connection);
-
