a689efafeba1ab7c9dcf9a89f15594015e2c6b39
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-12 10:18:13 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,23 +9,23 @@ VALUES
 INSERT INTO role (id, title, salary, department_id)
 VALUES
     (1,"Lead Engineer", 300000, 1),
-    (2,"Sales Lead", 100000, 2),
-    (3,"Account Manager", 160000, 3),
-    (4,"Lawyer", 190000, 4),
-    (5,"Software Engineer", 200000, 1),
-    (6,"Legal Team Lead", 250000, 4),
-    (7,"Sales Person", 80000, 2),
-    (8,"Accountant", 125000, 3);
+    (2,"Software Engineer", 200000, 1),
+    (3,"Sales Lead", 100000, 2),
+    (4,"Sales Person", 80000, 2),
+    (5,"Account Manager", 160000, 3),
+    (6,"Accountant", 125000, 3),
+    (7,"Legal Team Lead", 250000, 4),
+    (8,"Lawyer", 190000, 4);
 
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
     (1,"John", "Doe", 1, NULL),
-    (2,"Mike", "Chan", 5, 1),
-    (3,"Philip", "Gbeho",6, NULL),
+    (2,"Mike", "Chan", 2, 1),
+    (3,"Philip", "Gbeho",3, NULL),
     (4,"Sara", "Poppin", 4, 3),
-    (5,"Tom", "Allen", 2, NULL),
-    (6,"Josh", "Grey", 7, 5),
-    (7,"Walter", "Whyte", 3, NULL),
+    (5,"Tom", "Allen", 5, NULL),
+    (6,"Josh", "Grey", 6, 5),
+    (7,"Walter", "Whyte", 7, NULL),
     (8,"Tom", "Brown", 8, 7);
     
